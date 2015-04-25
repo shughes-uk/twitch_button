d1604f0e1211933d05984e4a30098f786df58e70
Samantha Hughes <shughes.uk@gmail.com>
2015-04-25 17:10:17 -0500

    More prelim stuff for button

--- stream_colorusb.py
+++ stream_colorusb.py
@@ -5,6 +5,7 @@ import pywinusb.hid as hid
 import thread
 import json
 from obsremote import OBSRemote
+import pythoncom, pyHook , sys
 
 class UsbButtonButton:
     def __init__(self,pressed_callback):
@@ -21,7 +22,7 @@ class UsbButtonButton:
                     device.open()
                     device.set_raw_data_handler(self.raw_handler)
 
-                for device in all_devices:                    
+                for device in all_devices:
                     print device
                     # browse feature reports
                     for report in device.find_feature_reports() + device.find_output_reports():
@@ -32,22 +33,24 @@ class UsbButtonButton:
             # if not usage_found:
             #     print("The target device was found, but the requested usage does not exist!\n")
 
+    def get_status(self):
+        for device in self.hid_devices:
+            for report in device.find_feature_reports() + device.find_output_reports():
+                report.send([0x00,0x02,0x00,0x00,0x00])
 
     def start(self):
         #init
-        filter = hid.HidDeviceFilter(vendor_id = 1994, product_id = 38992)
-        hid_device = filter.get_devices()
-        self.device = hid_device[0]
-        self.device.open()
-        self.device.set_raw_data_handler(self.press_handler)
-        self.target_usage = hid.get_full_usage_id(0xffa0, 0x02)
-        self.report = self.device.find_output_reports()[0] 
+        filter = hid.HidDeviceFilter(vendor_id = 0xd209)
+        self.hid_devices = filter.get_devices()
+        for device in self.hid_devices:
+            device.open()
+            device.set_raw_data_handler(self.raw_handler)
         return
 
     def stop(self):
         #stoppit
-        self.turn_off()
-        self.device.close()
+        for device in self.hid_devices:
+            device.close()
         return
 
     def raw_handler(self,data):
@@ -55,8 +58,8 @@ class UsbButtonButton:
 
     def press_handler(self,data):
         if data[2] == 1:
-            self.callback()         
-        #elif data[2] == 0:                       
+            self.callback()
+        #elif data[2] == 0:
         return
 
     def set_color(self,rgb):
@@ -64,6 +67,9 @@ class UsbButtonButton:
         self.report.send()
         return
 
+def OnKeyboardEvent(event):
+    print "Key: ", event.KeyID
+    return True
 
 
 buttonpressed = False
@@ -72,15 +78,22 @@ def callbk():
     buttonpressed = True
 
 if __name__ == '__main__':
-    #y = UsbButtonButton(callbk)
-    #y.find_devices()
-    obsRemote = OBSRemote("ws://192.168.1.107:4444")
-    obsRemote.start()
+    y = UsbButtonButton(callbk)
+    #obsRemote = OBSRemote("ws://192.168.1.107:4444")
+    #obsRemote.start()
+    y.start()
+
+    hm = pyHook.HookManager()
+    hm.KeyDown = OnKeyboardEvent
+    hm.HookKeyboard()
+    #pythoncom.PumpMessages()
     try:
         while True:
             sleep(0.1)
+            y.get_status()
+            pythoncom.PumpWaitingMessages()
     except KeyboardInterrupt:
         pass
     finally:
-        obsRemote.stop()
-        
+        #obsRemote.stop()
+        y.stop()
