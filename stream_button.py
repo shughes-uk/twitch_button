bd71093cc7bd55456b9b573981443ca8aa3aefd3
shughes <shughes.uk@gmail.com>
2015-05-03 12:11:56 +0100

    visual studio project

--- stream_button.py
+++ stream_button.py
@@ -41,4 +41,3 @@ if __name__ == '__main__':
         x.stop()
         y.turn_off()
         y.stop()
-
--- stream_colorusb.py
+++ stream_colorusb.py
@@ -13,15 +13,13 @@ class Manager(object):
         self.state = 'idle'
         self.streaming = False
         self.button = KeyboardButton()
-        self.obsRemote = None
+        self.obsremote = None
         self.current_profile = 0
-        self.last_button_pstate = False
+        
         self.nextstate = []
         return
 
     def next_profile(self):
-        print self.current_profile + 1 , len(self.profiles)
-        print (self.current_profile + 1) % len(self.profiles)
         self.current_profile = (self.current_profile + 1) % len(self.profiles)
         return self.current_profile
 
@@ -33,7 +31,7 @@ class Manager(object):
         if self.state == 'idle':
             if self.button.pressed:
                 self.state = 'profileselect_orstream'
-            
+
         elif self.state == 'profileselect_orstream':
             if self.button.pressed:
                if self.button.get_elapsed_time() > 5:
@@ -42,6 +40,7 @@ class Manager(object):
                     self.nextstate.append('streaming_idle')
             else:
                 self.next_profile();
+                print 'Selected next profile : %s' %self.profiles[self.current_profile]
                 self.state = 'idle'
         elif self.state == 'waitunpressed':
             if not self.button.pressed:
@@ -55,6 +54,12 @@ class Manager(object):
                     print "STOPPING STREAMING"
                     self.state = 'waitunpressed'
                     self.nextstate.append('idle')
+            else:
+                self.state = 'streaming_idle'
+    
+    def start_stream(self):
+        pass
+
 
 
 buttonpressed = False
@@ -70,7 +75,7 @@ if __name__ == '__main__':
 
     try:
         while True:
-            sleep(0.1)
+            sleep(0)
             y.tick()
             if statecache != y.state:
                 print y.state
