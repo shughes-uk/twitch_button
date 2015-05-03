ee39fb8d67c8501cb57726337345eacc305dfe6e
shughes <shughes.uk@gmail.com>
2015-05-03 12:49:35 +0100

    more prototyping

--- stream_colorusb.py
+++ stream_colorusb.py
@@ -13,9 +13,9 @@ class Manager(object):
         self.state = 'idle'
         self.streaming = False
         self.button = KeyboardButton()
-        self.obsremote = None
-        self.current_profile = 0
-        
+        self.obsremote = OBSRemote("ws://127.0.0.1:4444")
+        self.obsremote.start()
+        self.current_profile = 0        
         self.nextstate = []
         return
 
@@ -29,50 +29,78 @@ class Manager(object):
 
     def handle_state(self):
         if self.state == 'idle':
-            if self.button.pressed:
-                self.state = 'profileselect_orstream'
-
-        elif self.state == 'profileselect_orstream':
-            if self.button.pressed:
-               if self.button.get_elapsed_time() > 5:
-                    print "STARTING STREAM WITH PROFILE %s" %self.profiles[self.current_profile]
-                    self.state = 'waitunpressed'
-                    self.nextstate.append('streaming_idle')
-            else:
-                self.next_profile();
-                print 'Selected next profile : %s' %self.profiles[self.current_profile]
-                self.state = 'idle'
+            self.handle_idle()
+        elif self.state == 'profileselect':
+           self.handle_profileselect()
         elif self.state == 'waitunpressed':
-            if not self.button.pressed:
-                self.state = self.nextstate.pop()
+            self.handle_waitunpressed()
+        elif self.state == 'wait_streaming':
+           self.handle_wait_streaming()
+        elif self.state == 'wait_stop_streaming':
+           self.handle_wait_stop_streaming()
         elif self.state == 'streaming_idle':
-            if self.button.pressed:
-                self.state = 'streaming_pressed'
+            self.handle_streaming_idle()
         elif self.state == 'streaming_pressed':
-            if self.button.pressed:
-                if self.button.get_elapsed_time() > 2:
-                    print "STOPPING STREAMING"
-                    self.state = 'waitunpressed'
-                    self.nextstate.append('idle')
-            else:
-                self.state = 'streaming_idle'
+            self.handle_streaming_pressed()
+
+    def handle_idle(self):
+        if self.button.pressed:
+                self.state = 'profileselect'
+
+    def handle_profileselect(self):
+        if self.button.pressed:
+            if self.button.get_elapsed_time() > 5:
+                print "STARTING STREAM WITH PROFILE %s" %self.profiles[self.current_profile]
+                self.obsremote.set_profile(self.profiles[self.current_profile])
+                self.obsremote.start_streaming()
+                self.state = 'waitunpressed'                    
+                self.nextstate.append('streaming_idle')
+                self.nextstate.append('wait_streaming')
+        else:
+            self.next_profile();
+            print 'Selected next profile : %s' %self.profiles[self.current_profile]
+            self.state = 'idle'
+
+    def handle_waitunpressed(self):
+        if not self.button.pressed:
+                self.state = self.nextstate.pop()
+
+    def handle_wait_streaming(self):
+         if self.obsremote.streaming:
+                self.state = self.nextstate.pop()
+
+    def handle_wait_stop_streaming(self):
+         if not self.obsremote.streaming:
+                self.state = self.nextstate.pop()
+
+    def handle_streaming_idle(self):
+        if self.button.pressed:
+            self.state = 'streaming_pressed'
+        if not self.obsremote.streaming:
+            self.state = 'idle'
+
+    def handle_streaming_pressed(self):
+        if self.button.pressed:
+            if self.button.get_elapsed_time() > 2:
+                print "STOPPING STREAMING"
+                self.obsremote.stop_streaming()
+                self.state = 'waitunpressed'                    
+                self.nextstate.append('idle')
+                self.nextstate.append('wait_stop_streaming')
+
+        else:
+            self.state = 'streaming_idle'
+
     
+
     def start_stream(self):
         pass
-
-
-
-buttonpressed = False
-def callbk():
-    global buttonpressed
-    buttonpressed = True
 
 if __name__ == '__main__':
     y = Manager()
     y.button.daemon = True
     y.button.start()
     statecache = ''
-
     try:
         while True:
             sleep(0)
@@ -83,6 +111,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         pass
     finally:
-        #obsRemote.stop()
         y.button.stop()
         pass
