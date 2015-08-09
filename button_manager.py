525ee68192b81504657b53161cb0e036e2609a6f
shughes <shughes.uk@gmail.com>
2015-08-09 15:49:44 -0500

    logging

--- button_manager.py
+++ button_manager.py
@@ -235,6 +235,8 @@ class Manager(object):
             self.set_color(self.get_color(),"button")
             self.set_color((0, 0, 0),"tape")
         elif round(time()) % 2 == 0:
+            print self.button.current_color
+            print self.get_color()
             if self.twitch_handler:
                 if not self.twitch_handler.streamers[self.get_twitch_name()] and self.button.current_color != self.get_color():
                     self.set_color(self.get_color(),"button")
@@ -246,7 +248,7 @@ class Manager(object):
                     self.set_color((0, 255, 0),"tape")
         elif round(time()) % 2 == 1:
             if self.twitch_handler:
-                if not self.twitch_handler.streamers[self.get_twitch_name()] and self.button.current_color != [255,0,0]:
+                if not self.twitch_handler.streamers[self.get_twitch_name()] and self.button.current_color != (255,0,0):
                     self.set_color((255,0,0),"button")
                     self.set_color((255, 0, 0),"tape")
                     return
