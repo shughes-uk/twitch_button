589396ac60386a5cf8a995ef1f4ed99f161e7aa9
shughes <shughes.uk@gmail.com>
2015-10-11 18:08:59 -0500

    PEP8 all the things

--- obsremote.py
+++ obsremote.py
@@ -1,7 +1,12 @@
-import websocket, thread, json, logging, time
+import websocket
+import thread
+import json
+import logging
+
 
 class OBSRemote(object):
-    def __init__(self,url):
+
+    def __init__(self, url):
         self.logger = logging.getLogger("OBSRemote")
         self.url = url
         self.streaming = False
@@ -9,27 +14,27 @@ class OBSRemote(object):
         self.connected = True
 
     def start(self):
-            self.logger.info("Attempting to open comms with OBS")
-            self.ws = websocket.WebSocketApp(self.url,
-                                  on_message = self.on_message,
-                                  on_error = self.on_error,
-                                  on_close = self.on_close,
-                                  subprotocols=["obsapi"])
-            thread.start_new_thread( self.ws.run_forever, () )
-            self.connected = True
+        self.logger.info("Attempting to open comms with OBS")
+        self.ws = websocket.WebSocketApp(self.url,
+                                         on_message=self.on_message,
+                                         on_error=self.on_error,
+                                         on_close=self.on_close,
+                                         subprotocols=["obsapi"])
+        thread.start_new_thread(self.ws.run_forever, ())
+        self.connected = True
 
     def stop(self):
         self.logger.info("Closing comms with OBS")
         self.ws.close()
 
-    def on_message(self,ws,msg):
+    def on_message(self, ws, msg):
         try:
             decoded = json.loads(msg)
-            #self.logger.debug("Received: " + str(decoded))
+            # self.logger.debug("Received: " + str(decoded))
             if 'update-type' in decoded:
                 if decoded['update-type'] == 'StreamStatus':
                     self.streamTime = decoded['total-stream-time']
-                    self.streaming = decoded['streaming'] 
+                    self.streaming = decoded['streaming']
                 elif decoded['update-type'] == "StreamStarting":
                     self.streaming = True
                 elif decoded['update-type'] == "StreamStopping":
@@ -40,21 +45,21 @@ class OBSRemote(object):
             self.logger.warn(str(E))
         return
 
-    def on_error(self,ws,error):
+    def on_error(self, ws, error):
         if error.errno == 10061:
             self.logger.warn("Error, connection to OBS refused, check OBS is running.")
         else:
             self.logger.warn('Error ' + str(error))
         return
 
-    def on_close(self,ws):
+    def on_close(self, ws):
         self.logger.info('Socket closed')
         self.connected = False
         self.streaming = False
         return
 
-    def set_profile(self,name):
-        self.logger.info("Setting profile to : %s" %name)
+    def set_profile(self, name):
+        self.logger.info("Setting profile to : %s" % name)
         msg = {}
         msg['message-id'] = 'ffff34234'
         msg['request-type'] = "SetProfile"
@@ -62,14 +67,14 @@ class OBSRemote(object):
         self.ws.send(json.dumps(msg))
 
     def start_streaming(self, preview=False):
-        if self.streaming == False:
+        if not self.streaming:
             self.logger.info("Sending StartStream")
             msg = {}
             msg['message-id'] = "123123d"
             msg['request-type'] = "StartStopStreaming"
             if preview:
                 msg["preview-only"] = True
-            response = self.ws.send(json.dumps(msg))
+            self.ws.send(json.dumps(msg))
 
     def stop_streaming(self, preview=False):
         if self.streaming:
@@ -79,5 +84,5 @@ class OBSRemote(object):
             msg['request-type'] = "StartStopStreaming"
             if preview:
                 msg["preview-only"] = True
-            response = self.ws.send(json.dumps(msg))
+            self.ws.send(json.dumps(msg))
         return
